ef9da5adbf05f45d937cbe66c4834a2e217841c8
ATai2 <1114677814@qq.com>
2020-06-02 00:08:59 +0800

    init

---
+++ sqltrans-core/src/test/java/com/ppx/sqltrans/SqlCastTest.java
@@ -0,0 +1,24 @@
+package com.ppx.sqltrans;
+
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.support.json.JSONUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqlCastTest {
+
+    @Test
+    public void mysql2Oracle() {
+    }
+
+    @Test
+    public void showDatabases() {
+        String mysqlStr = "show databases;";
+//        SQLUtils.parseStatements(mysqlStr, );
+        SQLExpr sqlExpr = SQLUtils.toMySqlExpr(mysqlStr);
+
+        System.out.println(JSONUtils.toJSONString(sqlExpr));
+    }
+}
