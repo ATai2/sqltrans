f79a9324c2eca5fde0cad5fb8378a33edfcbabf6
atai2 <1114677814@qq.com>
2020-08-10 23:12:40 +0800

    ssssss

---
+++ at-tools/src/test/java/com/at/tool/IpGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.at.tool;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class IpGeneratorTest extends TestCase {
+
+    @Test
+    public void test(){
+        System.out.println(IpGenerator.getIp());
+    }
+
+}
